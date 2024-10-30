23a646d0a7b67276d39ac29c991bd0cc3d7f625e
Vishal Chaure <vishal.chaure22@spit.ac.in>
2024-10-30 14:38:28 +0530

    it is added?

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['assets.aceternity.com'],
+  },
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en">  
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} dark`}
       >
         {children}
       </body>
